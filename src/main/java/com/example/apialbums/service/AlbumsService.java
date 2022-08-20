34b199e5bf24fc47d5f59f0c5a34920e5c0bc7f4
ElenaGrib <elenagrib27@gmail.com>
2022-08-20 16:46:49 +0300

    Initial commit

--- src/main/java/com/example/apialbums/service/AlbumsService.java
+++ src/main/java/com/example/apialbums/service/AlbumsService.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.appsdeveloperblog.photoapp.api.albums.service;
+package com.example.apialbums.service;
 
+import com.example.apialbums.data.AlbumEntity;
 
-import com.appsdeveloperblog.photoapp.api.albums.data.AlbumEntity;
 import java.util.List;
 
 public interface AlbumsService {
--- src/main/java/com/example/apialbums/service/AlbumsServiceImpl.java
+++ src/main/java/com/example/apialbums/service/AlbumsServiceImpl.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.appsdeveloperblog.photoapp.api.albums.service;
+package com.example.apialbums.service;
 
-import com.appsdeveloperblog.photoapp.api.albums.data.AlbumEntity;
+import com.example.apialbums.data.AlbumEntity;
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.stereotype.Service;
 
 @Service
 public class AlbumsServiceImpl implements AlbumsService {
@@ -16,25 +17,25 @@ public class AlbumsServiceImpl implements AlbumsService {
     @Override
     public List<AlbumEntity> getAlbums(String userId) {
         List<AlbumEntity> returnValue = new ArrayList<>();
-        
+
         AlbumEntity albumEntity = new AlbumEntity();
         albumEntity.setUserId(userId);
         albumEntity.setAlbumId("album1Id");
         albumEntity.setDescription("album 1 description");
         albumEntity.setId(1L);
         albumEntity.setName("album 1 name");
-        
+
         AlbumEntity albumEntity2 = new AlbumEntity();
         albumEntity2.setUserId(userId);
         albumEntity2.setAlbumId("album2Id");
         albumEntity2.setDescription("album 2 description");
         albumEntity2.setId(2L);
         albumEntity2.setName("album 2 name");
-        
+
         returnValue.add(albumEntity);
         returnValue.add(albumEntity2);
-        
+
         return returnValue;
     }
-    
+
 }
