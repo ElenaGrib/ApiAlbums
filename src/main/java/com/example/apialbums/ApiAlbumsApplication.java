aaefabe4b0e2398d5d6809f7433f8f7d9806177f
ElenaGrib <elenagrib27@gmail.com>
2022-08-20 16:42:01 +0300

    Initial commit

---
+++ src/main/java/com/example/apialbums/ApiAlbumsApplication.java
@@ -0,0 +1,13 @@
+package com.example.apialbums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAlbumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAlbumsApplication.class, args);
+    }
+
+}
